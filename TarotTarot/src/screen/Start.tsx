ce29820e4f7953e0faeecf885bf35b6ca5459058
SebinLee <sebinlee.artdev@gmail.com>
2023-11-21 14:33:37 +0900

    [Bug Fix] Add Stack Navigation Dependencies

--- TarotTarot/src/screen/Start.tsx
+++ TarotTarot/src/screen/Start.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View } from 'react-native'
 import { Screen } from '../design/Layout'
 import FastImage from 'react-native-fast-image'
 import StyledButton from '../design/Button'
 import { ButtonSize } from '../design/button/ButtonInterface'
 import { NavigationPropEnum } from '../design/layout/LayoutInterface'
+import TextInput from '../design/TextInput'
 
 export default function Start() {
+
+    const [text, setText] = useState("")
+
     return (
         <Screen title="test" navigationLeftProp={NavigationPropEnum.back} navigationRightProp={NavigationPropEnum.close}>
             <View>
@@ -16,6 +20,7 @@ export default function Start() {
                     resizeMode={FastImage.resizeMode.contain}
                 />
                 <StyledButton size={ButtonSize.L} text="시작하기"/>
+                <TextInput value={text} onChangeText={(text) => setText(text)} multiline={true}/>
             </View>
         </Screen>
     )
