b7220e388410f8249365cc0752dce72c13cd1a0c
SebinLee <sebinlee.artdev@gmail.com>
2023-11-23 14:13:04 +0900

    Add TarotCard Component (Work Needed)

---
+++ TarotTarot/src/design/TarotCard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FastImage from "react-native-fast-image";
+
+export default function TarotCard() {
+    return <FastImage />;
+}
+
+export enum TarotCardType {
+    "TheFool",
+}
