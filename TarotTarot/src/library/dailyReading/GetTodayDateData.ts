03b14601471824fda340bd3291406302ac945b2c
SebinLee <sebinlee.artdev@gmail.com>
2023-12-04 00:51:25 +0900

    Edit GetTodayDateData to handle days before 10

--- TarotTarot/src/library/dailyReading/GetTodayDateData.ts
+++ TarotTarot/src/library/dailyReading/GetTodayDateData.ts
@@ -2,13 +2,16 @@ import { DateData } from "react-native-calendars";
 
 export default function GetTodayDateData(): DateData {
     const today = new Date();
+    const todayDate =
+        today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
+
     return {
         year: today.getFullYear(),
         month: today.getMonth() + 1,
         day: today.getDate(),
         dateString: `${today.getFullYear()}-${
             today.getMonth() + 1
-        }-${today.getDate()}`,
+        }-${todayDate}`,
         timestamp: today.getTime(),
     };
 }
